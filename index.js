7d3de67f2350d59d7a8be884847ba8d539b6fb0f
mixalezhnev <mixa.lezhnev@gmail.com>
2017-11-26 20:35:26 +0400

    Play with request module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
+const request = require('request-promise')
 
 const app = express()
 const port = 3001
@@ -8,47 +9,27 @@ const port = 3001
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
-  layoutDir: path.join(__dirname, 'views/layouts')
+  layoutsDir: path.join(__dirname, 'views/layouts')
 }))
-
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/user/:id', (req, res) => {
-  res.render('home', {
-    name: req.params.id,
-  });
-});
-
-
-app.get('/file/:name', (req, res, next) => {
-  const options = {
-    root: __dirname + '/public/',
-    dotfiles: 'deny',
-    headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true
-    }
-  };
-
-  const fileName = req.params.name;
-  res.sendFile(fileName, options, err => {
-    if (err)
-      next(err);
-    else {
-      console.log('Sent: ', fileName);
-    }
-  });
-});
-
-app.use((err, req, res, next) => {
-  console.log(err)
-  // res.status(500).json({msg: 'Something went wrong'})
+app.get('/:city', (req, res) => {
+  request({
+    uri: 'http://apidev.accuweather.com/locations/v1/search',
+    qs: {
+      q: req.params.city,
+      apiKey: '295382'
+    },
+    json: true
+  })
+  .then(data => {
+    res.render('index', data)
+  })
+  .catch(e => {
+    console.log(e);
+    res.render('error')
+  })
 })
 
-app.listen(port, err => {
-  if (err)
-    return console.log('somethnig bad happened', err);
-
-  console.log(`server is listening on ${port}`);
-})
+app.listen(port)
